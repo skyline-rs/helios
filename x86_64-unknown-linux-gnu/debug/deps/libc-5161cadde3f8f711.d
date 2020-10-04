08c68bbe42cd23a06586083f0baba2c757d27d86
GitHub Action <action@github.com>
2020-10-04 23:07:27 +0000

    Add Documentation

---
+++ x86_64-unknown-linux-gnu/debug/deps/libc-5161cadde3f8f711.d
@@ -0,0 +1,5 @@
+/home/runner/work/helios/helios/./x86_64-unknown-linux-gnu/debug/deps/libc-5161cadde3f8f711.rmeta: /home/runner/.cargo/git/checkouts/libc-nnsdk-887c2406398ea5ab/4f2c257/src/lib.rs
+
+/home/runner/work/helios/helios/./x86_64-unknown-linux-gnu/debug/deps/libc-5161cadde3f8f711.d: /home/runner/.cargo/git/checkouts/libc-nnsdk-887c2406398ea5ab/4f2c257/src/lib.rs
+
+/home/runner/.cargo/git/checkouts/libc-nnsdk-887c2406398ea5ab/4f2c257/src/lib.rs:
---
+++ x86_64-unknown-linux-gnu/debug/deps/skyline-d5bdb7b15aa126dd.d
@@ -0,0 +1,15 @@
+/home/runner/work/helios/helios/./x86_64-unknown-linux-gnu/debug/deps/skyline-d5bdb7b15aa126dd.rmeta: /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/lib.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/hooks.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/logging.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/patching.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/text_iter.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/error.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/nro.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/info.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/extern_alloc.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/build.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/nn.rs
+
+/home/runner/work/helios/helios/./x86_64-unknown-linux-gnu/debug/deps/skyline-d5bdb7b15aa126dd.d: /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/lib.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/hooks.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/logging.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/patching.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/text_iter.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/error.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/nro.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/info.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/extern_alloc.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/build.rs /home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/nn.rs
+
+/home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/lib.rs:
+/home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/hooks.rs:
+/home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/logging.rs:
+/home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/patching.rs:
+/home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/text_iter.rs:
+/home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/error.rs:
+/home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/nro.rs:
+/home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/info.rs:
+/home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/extern_alloc.rs:
+/home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/build.rs:
+/home/runner/.cargo/git/checkouts/skyline-rs-652bad0610bd03a8/cfdcf94/src/nn.rs:

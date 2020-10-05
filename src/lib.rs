09da7d437e8030a8f631b2ae1b167b5e22872762
jam1garner <8260240+jam1garner@users.noreply.github.com>
2020-10-05 17:18:07 -0400

    Cleanup base

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,14 @@
 use std::fs;
 use std::io;
 use std::net::IpAddr;
-use std::path::{ Path, PathBuf };
+use std::path::{Path, PathBuf};
 
-use serde::{ Deserialize, Serialize };
+use serde::{Deserialize, Serialize};
 
 pub use update_protocol::UpdateResponse;
 
-#[macro_export]
-macro_rules! get_helios_path {
-    () => {
-        // Not too sure about that result
-        //format!("sd:/helios/{}", skyline::info::get_program_id())
-        "sd:/helios/01006A800016E000"
-    };
+pub fn get_helios_path() -> PathBuf {
+    PathBuf::from(format!("sd:/helios/{:016X}", skyline::info::get_program_id()))
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -25,86 +20,44 @@ pub struct Config {
     pub server_ip: IpAddr,
 }
 
-pub struct HeliosInstaller;
-
-impl skyline_update::Installer for HeliosInstaller {
-    fn should_update(&self, response: &UpdateResponse) -> bool {
-        let result = skyline_web::Dialog::yes_no(format!(
-            "An update for {} has been found.\n\nWould you like to download it?",
-            response.plugin_name
-        ));
-
-        // If the user accepted the update
-        if result {
-            if let Some(new_version) = &response.new_plugin_version {
-                // Get the configuration path using the plugin's name
-                let config_path = format!("{}/{}.toml", get_helios_path!(), response.plugin_name);
-
-                // Open the current configuration to edit it
-                let mut config = open_config_toml(&config_path).unwrap();
-                config.version = new_version.to_string();
-
-                // Update the configuration
-                update_config_toml(&config_path, &config);
-            }
-        }
-
-        result
-    }
-
-    fn install_file(&self, path: PathBuf, buf: Vec<u8>) -> Result<(), ()> {
-        let _ = std::fs::create_dir_all(path.parent().ok_or(())?);
-        if let Err(e) = std::fs::write(path, buf) {
-            println!("[updater] Error writing file to sd: {}", e);
-            Err(())
-        } else {
-            Ok(())
-        }
-    }
-}
-
-pub fn file_discovery() -> Result<(), io::Error>{
+pub fn file_discovery() -> Result<Vec<(UpdateResponse, PathBuf)>, io::Error> {
     // Get Helios path for current Application
-    let helios_path = PathBuf::from(get_helios_path!());
+    let helios_path = get_helios_path();
     println!("[helios] Path to discover: {:?}", helios_path);
 
     // Does the directory exist?
     if !helios_path.exists() {
-        return Err(io::Error::new(io::ErrorKind::NotFound, "[helios] No directory found for this application"));
+        fs::create_dir_all(&helios_path)?;
     }
-
-    println!("Path exists");
-
-    // Is it actually a file?
-    // if helios_path.is_file() {
-    //     return Err(io::Error::new(io::ErrorKind::Other, "[helios] Path refers to a file instead of a directory"));
-    // }
 
     // Iterate through all the DirEntries
-    for entry in fs::read_dir(helios_path)? {
-        let entry = entry?;
+    fs::read_dir(helios_path)?
+        .map(|entry| {
+            let entry = entry?;
 
-        //Ignore directories (for now?)
-        if entry.file_type().unwrap().is_dir() {
-            continue;
-        }
+            //Ignore directories (for now?)
+            if entry.file_type().unwrap().is_dir() {
+                return Ok(None);
+            }
 
-        println!("About to read configuration");
+            println!("About to read configuration");
 
-        // Read the configuration
-        let config: Config = open_config_toml(&entry.path()).unwrap();
+            // Read the configuration
+            let config: Config = open_config_toml(&entry.path()).unwrap();
 
-        println!("Configuration read successfully");
+            println!("Configuration read successfully");
 
-        println!("About to ask server");
-        // Ask the update server if an update is available and call our custom installer that will take care of updating the configuration
-        skyline_update::custom_check_update(config.server_ip, &config.name, &config.version, false, &HeliosInstaller);
+            println!("About to ask server");
+            // Ask the update server if an update is available and call our custom installer that will take care of updating the configuration
+            
+            let update = skyline_update::get_update_info(config.server_ip, &config.name, &config.version, false);
 
-        println!("Finished asking server");
+            println!("Finished asking server");
 
-    }
-
-    Ok(())
+            Ok(update.map(|x| (x, entry.path())))
+        })
+        .filter_map(|x| x.transpose())
+        .collect()
 }
 
 
